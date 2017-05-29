a8439f1280974489c698ab9cbe41e6e3dd014bab
Robin <robda446@student.liu.se>
2017-05-29 20:44:52 +0200

    Added SoundManager

---
+++ BombermanGame/SoundHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombermanGame
+{
+    static class SoundHandler
+    {
+        private static SoundPlayer themeMusic = new SoundPlayer(Properties.Resources.theme1);
+
+        public static void PlayTheme() { themeMusic.Play(); }
+        public static void StopTheme() { themeMusic.Stop(); }
+
+
+
+        public static void PlayExplosionSound() {
+            SoundPlayer themeMusic = new SoundPlayer(Properties.Resources.theme1);
+        } 
+
+
+    }
+}
