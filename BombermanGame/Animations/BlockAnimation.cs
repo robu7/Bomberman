2b6942ccf0422972dde9773dc22b0b7e3abc273f
Robin <robda446@student.liu.se>
2017-07-02 20:04:51 +0200

    Added Block destruct animation

---
+++ BombermanGame/Animations/BlockAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using SharpDX.Direct2D1;
+
+namespace BombermanGame.Animations
+{
+    class BlockAnimation : IGraphicsResourceLoader
+    {
+        static private List<SharpDX.Direct2D1.Bitmap> destruction;
+
+        public void LoadGraphics(RenderTarget target)
+        {
+            destruction = new List<SharpDX.Direct2D1.Bitmap>();
+
+            System.Drawing.Bitmap original = Properties.Resources.BlockDestruction;
+            System.Drawing.Bitmap sprite;
+            Rectangle srcRect;
+
+            // Init down
+
+            for (int i = 0; i < 4; ++i) {
+                srcRect = new Rectangle(i * 87, 0, 87, 87);
+                sprite = original.Clone(srcRect, original.PixelFormat);
+                destruction.Add(sprite.CreateDirectX2D1Bitmap(target));
+                sprite.Dispose();
+            }
+        }
+
+        public static Animation GetDestroyAnimation()
+        {
+            return new Animation(destruction, 1.2, false);
+        }
+
+    }
+}
--- BombermanGame/GameObjects/Block.cs
+++ BombermanGame/GameObjects/Block.cs
@@ -2,26 +2,47 @@
 using BombermanGame.GameObjects;
 using System;
 using BombermanGame.GameObjects.Powerups;
+using BombermanGame.Animations;
 
 namespace BombermanGame {
     class Block : GameObject {
         private BitmapLoader loader = new BlockGraphicsLoader();
         public Powerup HiddenItem = null;
+        private Animation spriteAnimation;
+        private int? TimeToLive = null;
 
         public override void Draw(RenderTarget target) {
             var b = Tile.Bounds;
-            target.DrawBitmap(loader.Bitmap, new SharpDX.Mathematics.Interop.RawRectangleF(b.Left, b.Top, b.Right, b.Bottom), 1, SharpDX.Direct2D1.BitmapInterpolationMode.Linear);
+            if (spriteAnimation?.State == AnimationState.InProgress) {
+                target.DrawBitmap(spriteAnimation.CurrentFrame, new SharpDX.Mathematics.Interop.RawRectangleF(b.Left, b.Top, b.Right, b.Bottom), 1, SharpDX.Direct2D1.BitmapInterpolationMode.Linear);
+            } else {
+                target.DrawBitmap(loader.Bitmap, new SharpDX.Mathematics.Interop.RawRectangleF(b.Left, b.Top, b.Right, b.Bottom), 1, SharpDX.Direct2D1.BitmapInterpolationMode.Linear);
+            }
         }
 
         public override void Update(double totalTime) {
             // No need to do anything here
+            if (TimeToLive.HasValue) {
+                if (spriteAnimation.State == AnimationState.Stopped) {
+                    base.OnDestroy(totalTime);
+                    Tile.Object = HiddenItem;
+                    HiddenItem?.SpawnedFromBlock(totalTime);
+                } else
+                    spriteAnimation.Update(totalTime);
+
+                Tile.MarkAsDirty();
+            }
         }
 
         protected override void OnDestroy(double currentTime)
         {
-            base.OnDestroy(currentTime);
-            Tile.Object = HiddenItem;
-            HiddenItem?.SpawnedFromBlock(currentTime);          
+            //base.OnDestroy(currentTime);
+            //Tile.Object = HiddenItem;
+            //HiddenItem?.SpawnedFromBlock(currentTime);
+            spriteAnimation = BlockAnimation.GetDestroyAnimation();
+            spriteAnimation.Start(currentTime);
+            TimeToLive = 4;
+
         }
     }
 
